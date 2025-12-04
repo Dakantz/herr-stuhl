34e5523e85e414c7741bb54e4b596e4500e7eb7c
Benedikt Kantz <benedikt@kantz.at>
2025-12-05 00:31:31 +0100

    herr stuhl

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Stool from './components/Stool.vue';
+</script>
+
+<template>
+
+  <div>
+    <div class="main_description">
+      HERR
+    </div>
+    <Stool />
+  </div>
+  <div class="main_description">
+    STUHL
+  </div>
+  <div>
+    <a href="https://dakantz.at" target="_blank">dakantz.at</a>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: "IBM Plex Mono";
+  src: url('/fonts/IBM_Plex_Mono/IBMPlexMono-Medium.ttf') format('truetype');
+}
+
+body {
+  font-family: "IBM Plex Mono", serif;
+  font-optical-sizing: auto;
+  font-weight: 200;
+  font-style: normal;
+  padding: 0;
+  background-color: #9da9b7;
+}
+
+.main_description {
+  color: #7bd185;
+}
+</style>
---
+++ src/components/Stool.vue
@@ -0,0 +1,156 @@
+<script setup lang="ts">
+import * as THREE from 'three';
+
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { onMounted, ref } from 'vue';
+import { LineMaterial, Wireframe, WireframeGeometry2 } from 'three/examples/jsm/Addons.js';
+import { update } from 'three/examples/jsm/libs/tween.module.js';
+
+const stool_ref = ref<HTMLElement | null>(null);
+defineProps<{}>()
+
+
+let camera: THREE.OrthographicCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, controls: OrbitControls;
+let rotation_speed = 0.01;
+let angle = 0;
+onMounted(() => {
+  init();
+
+  setInterval(() => {
+    angle += rotation_speed;
+    scene_objects.forEach((obj) => {
+      obj.rotation.x = angle;
+    });
+    update();
+  }, 10);
+});
+const scene_objects: THREE.Object3D[] = [];
+async function init() {
+  // aspect
+  let rect = stool_ref.value?.getBoundingClientRect();
+  if (!rect) return;
+  let aspect = rect.width / rect.height;
+  let size = 10;
+  camera = new THREE.OrthographicCamera(-size / 2, size / 2, size * aspect / 2, -size * aspect / 2, -2, 200);
+
+  camera.position.set(-1, 1, 1);
+  camera.lookAt(0, 0, 0);
+  // scene
+
+  scene = new THREE.Scene();
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 10);
+  scene.add(ambientLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+  camera.add(pointLight);
+  scene.add(camera);
+
+  // model
+
+  const mtlLoader = new MTLLoader().setPath('/models/');
+  const materials = await mtlLoader.loadAsync('stool.mtl');
+  materials.preload();
+
+  const objLoader = new OBJLoader().setPath('/models/');
+  objLoader.setMaterials(materials); // optional since OBJ assets can be loaded without an accompanying MTL file
+
+  const object = await objLoader.loadAsync('stool.obj');
+  let avg = new THREE.Vector3();
+  let count = 0;
+  for (let vert of object.children) {
+    if ((vert as THREE.Mesh).isMesh) {
+      let position = (vert as THREE.Mesh).geometry.attributes.position;
+      if (!position) continue;
+      for (let i = 0; i < position.count; i++) {
+        let vertex = new THREE.Vector3().fromBufferAttribute(position, i);
+        avg.add(vertex);
+        count++;
+      }
+    }
+  }
+  avg.divideScalar(count);
+  let offset = avg.multiplyScalar(-1);
+  object.traverse((child) => {
+    if ((child as THREE.Mesh).isMesh) {
+
+      const geometry = new WireframeGeometry2((child as THREE.Mesh).geometry);
+
+      let matLine = new LineMaterial({
+
+        color: 0x000000,
+        linewidth: 2, // in pixels
+        dashed: true
+
+      });
+
+      let wireframe = new Wireframe(geometry, matLine);
+      wireframe.computeLineDistances();
+      wireframe.scale.set(1, 1, 1);
+      wireframe.position.copy(offset);
+      scene.add(wireframe);
+      scene_objects.push(wireframe);
+    }
+  });
+  object.position.copy(offset);
+  scene.add(object);
+  scene_objects.push(object);
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(rect.width, rect.height);
+  renderer.setAnimationLoop(animate);
+  renderer.setClearColor(0x000000ff, 0);
+  const container = stool_ref.value;
+  if (container) {
+    container.appendChild(renderer.domElement);
+  }
+
+  //
+
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
+
+}
+
+function onWindowResize() {
+  let rect = stool_ref.value?.getBoundingClientRect();
+  if (!rect) return;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(rect.width, rect.height);
+  const container = stool_ref.value;
+  if (container) {
+    container.appendChild(renderer.domElement);
+  }
+}
+
+function animate() {
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+
+}
+
+</script>
+
+<template>
+  <div id="stool-container" ref="stool_ref">
+
+
+  </div>
+</template>
+
+<style scoped>
+#stool-container {
+  width: 80vh;
+  height: 80vh;
+  overflow: hidden;
+}
+</style>
